9dd9cd5c75780f41a30d2ae54453c62017ea5e91
Yusuf <danalungu297@gmail.com>
2026-02-12 21:44:52 +0200

    i did this change

---
+++ src/app/icloudadmin/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'YSF-Panel | Icloud',
+  description: 'Admin panel for iCloud connections.',
+  icons: {
+    icon: '/adminlogo.png',
+    apple: '/adminlogo.png',
+  },
+};
+
+export default function IcloudAdminLayout({ children }) {
+  return children;
+}
